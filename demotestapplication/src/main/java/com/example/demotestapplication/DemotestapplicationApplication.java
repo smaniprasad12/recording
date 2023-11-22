ded55f02fe865db4ee175655bf5c8c98d9000187
siddpras <siddpras@inlen8520019911.corp.capgemini.com>
2023-11-22 14:39:22 +0530

    second commit

---
+++ demotestapplication/src/main/java/com/example/demotestapplication/DemotestapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.example.demotestapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemotestapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemotestapplicationApplication.class, args);
+	}
+
+}
---
+++ demotestapplication/src/test/java/com/example/demotestapplication/DemotestapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demotestapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemotestapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
